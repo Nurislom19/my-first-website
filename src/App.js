2dda9e819d2f82c96a4c807a7c5c4a6af0088526
Nurislom19 <155104773+Nurislom19@users.noreply.github.com>
2023-12-29 01:36:12 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmployeeList from './EmployeeList'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Мое приложение для управления сотрудниками</h1>
+      <EmployeeList />
+    </div>
+  )
+}
+
+export default App
